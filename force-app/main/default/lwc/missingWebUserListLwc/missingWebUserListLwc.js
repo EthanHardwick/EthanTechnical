5a4494c63c29a7505ba3db96e837a78b5de7b4fe
Ethan Hardwick <hardwick.ethan@outlook.com>
2024-01-30 12:23:52 +0000

    connected Ui to Apex and added visual validation

--- force-app/main/default/lwc/missingWebUserListLwc/missingWebUserListLwc.js
+++ force-app/main/default/lwc/missingWebUserListLwc/missingWebUserListLwc.js
@@ -1,15 +1,41 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import fetchMissingUsers from '@salesforce/apex/MissingWebUserListCtrl.fetchMissingUsers';
 
 const COLUMNS = [
             {label: 'Id', fieldName: 'id', type: 'text'},
             {label: 'Name', fieldName: 'name', type: 'text'},
             {label: 'UserName', fieldName: 'userName', type: 'text'},
             {label: 'Email', fieldName: 'email', type: 'email'},
-            {label: 'Company Name', fieldName: 'company', type: 'text'}
+            {label: 'Company Name', fieldName: 'companyName', type: 'text'}
         ]
+
 export default class MissingWebUserListLwc extends LightningElement {
+
+  data = [];
+  spinnerActive = true;
+
+  @wire(fetchMissingUsers, {})
+  getMissingWebUsers({error, data}){
+    if (data) {
+      this.data = data;
+      this.spinnerActive = false;
+    } else if (error) {
+      this.showNotification('An error occurred when fetching users', error.body.message, 'error');
+    }
+  }
 
   get columns(){
     return COLUMNS;
+  }
+
+  showNotification(title, message, variant) {
+    this.dispatchEvent(
+        new ShowToastEvent({
+        title: title,
+        message: message,
+        variant: variant,
+      })
+    );
   }
 }
